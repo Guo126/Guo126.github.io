813864a9b412123f941876df227ce0e4258cba6c
wenhao.guo <wenhao.guo@definesys.com>
2019-08-20 15:53:30 +0800

    happy!

--- router/router.js
+++ router/router.js
@@ -1,22 +1,29 @@
 new Guo({
-    routes: [{
-        path: '/home',
-        component: '../components/home/home.html',
-        //title: '一鲸落 万物生'
-    }, {
-        path: '/demo',
-        component: '../components/demoPanel/index.html'
-        
-    }, {
-        path: '/work',
-        component: '../components/workPanel/index.html'
-        
-    }, {
-        path: '/talk',
-        component: '../components/talkPanel/index.html'
-        
-    }, {
-        path: '/',
-        redirect: '/home'
-    }]
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: '../components/home/home.html',
+            //title: '一鲸落 万物生'
+        }, {
+            path: '/demo',
+            component: '../components/demoPanel/index.html'
+
+        }, {
+            path: '/work',
+            component: '../components/workPanel/index.html'
+
+        }, {
+            path: '/talk',
+            component: '../components/talkPanel/index.html'
+
+        },
+        {
+            path: '/book',
+            component: '../components/bookPanel/index.html'
+
+        },]
 });
